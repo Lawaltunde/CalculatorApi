c5c8b157dcfead39565aa3df5bc7e0986c11cf64
Olatunde <122512872+Lawaltunde@users.noreply.github.com>
2024-02-10 21:30:12 +0000

    5 unit tests added for the add logic

--- CalculatorMsUnitTests/AddLogicUnitTest.cs
+++ CalculatorMsUnitTests/AddLogicUnitTest.cs
@@ -1,16 +1,52 @@
+using MyApp.Namespace;
 using StringLibrary;
-using  CalculatorUnitTestLibrary;
 namespace CalculatorMsUnitTests;
 
 [TestClass]
 public class AddLogicUnitTest
 {
     [TestMethod]
-    public void TestMethod1()
+    public void AddShouldReturnCorrectSum()
     {
         var firstNumber = 5;
         var secondNumber = 7;
-        var result = Add(firstNumber, secondNumber);
+        var result = Calculator.Add(firstNumber, secondNumber);
+       Assert.IsTrue(result, $"The result of {result} should have been false for sum of {firstNumber} and {secondNumber}");
+    }
+
+    [TestMethod]
+    public void WhenBothValuesAreZeroesReturnShouldBeZero()
+    {
+        var firstNumber = 0;
+        var secondNumber = 0;
+        var result = Calculator.Add(firstNumber, secondNumber);
+       Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void ZeroesShouldReturnedWhenNoValueIsProvided()
+    {
+        var firstNumber;
+        var secondNumber;
+        var result = Calculator.Add(firstNumber, secondNumber);
+       Assert.IsTrue(result, $"The result of {result} should have been false for sum of {firstNumber} and {secondNumber}");
+    }
+
+    [TestMethod]
+    public void Add_ShouldHandleNegativeNumbers()
+    {
+        var firstNumber = -5;
+        var secondNumber = 7;
+        var result = Calculator.Add(firstNumber, secondNumber);
+       Assert.IsTrue(2, result);
+    }
+
+    [TestMethod]
+    public void Add_ShouldReturnCorrectAnswerWhenPassedValuesAreDecimals()
+    {
+        var firstNumber = 5.98;
+        var secondNumber = 7.98;
+        var result = Calculator.Add(firstNumber, secondNumber);
        Assert.IsTrue(result, $"The result of {result} should have been false for sum of {firstNumber} and {secondNumber}");
     }
 }
