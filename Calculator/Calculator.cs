39ae052aa503a799c8f4090aec5d886cad35457f
Olatunde <122512872+Lawaltunde@users.noreply.github.com>
2024-02-14 05:22:48 +0000

    donegit add .!

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -5,28 +5,28 @@ namespace MyApp.Namespace
 {
     [Route("[controller]/[action]")]
     [ApiController]
-    public static class Calculator : ControllerBase
+    public class Calculator : ControllerBase
     {
         //[Route("{num1}/{num2}")]
 
         public static decimal answer =0;
         //addition
 
-        public static String Add(decimal num1, decimal num2){
+        public  String Add(decimal num1, decimal num2){
             return (answer = num1 + num2).ToString();
         }
         //subtraction
-        public static String Subtract(decimal num1, decimal num2){
+        public  String Subtract(decimal num1, decimal num2){
             return (answer = num1 - num2).ToString();
         }
 
         //multiplication
-         public static String Multiply(decimal num1, decimal num2){
+         public  String Multiply(decimal num1, decimal num2){
             return (answer = num1 * num2).ToString();
         }
         
         //division
-         public static String Divide(decimal num1, decimal num2){
+         public  String Divide(decimal num1, decimal num2){
             if(!num2.Equals(0)){
                 return (answer = num1 / num2).ToString();
             }
@@ -35,7 +35,7 @@ namespace MyApp.Namespace
         }
 
         //modulus
-         public static String Modulo(decimal num1, decimal num2){
+         public String Modulo(decimal num1, decimal num2){
             if(num1.Equals(0))
                 return (answer = 0).ToString();
             else 
